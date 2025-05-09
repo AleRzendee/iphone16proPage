bf416129aa64c2b7ea9add54970877418401c27b
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-05-09 01:57:50 -0300

    Basic Strucuture; (#007)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
-import Navbar from "./components/layouts/Navbar.jsx"
+import Navbar from "./components/layouts/Navbar.jsx";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
 import Model from "./components/Model";
 import Features from "./components/Features";
 import HowItWorks from "./components/HowItWorks";
 import Footer from "./components/Footer";
-import React from 'react';
 
 import * as Sentry from "@sentry/react";
 import AppleIntelligence from "./components/AppleIntelligence";
 
 const App = () => {
-  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
-
   return (
     //there was an error of horizontal scrolling fixed using this ,dont remove this
     <main className="overflow-x-hidden">
