deb4303d3ac8145a42498b223dc1e6aaf165e3b4
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-05-09 02:08:01 -0300

    Acessories Component; (#025)

---
+++ src/components/pages/Accessories.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../layouts/Navbar";
+import Footer from "../Footer";
+
+const Accessories = () => {
+  return (
+    <div>
+      <Navbar />
+      Accessories
+      <Footer />
+    </div>
+  );
+};
+
+export default Accessories;
