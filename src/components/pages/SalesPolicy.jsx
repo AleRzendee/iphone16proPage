24fee9696f40a6988e87f53878302238c326c2b3
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-05-09 02:07:23 -0300

    SLP Component; (#023)

---
+++ src/components/pages/SalesPolicy.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../layouts/Navbar";
+import Footer from "../Footer";
+
+const SalesPolicy = () => {
+  return (
+    <div>
+      <Navbar />
+      SalesPolicy
+      <Footer />
+    </div>
+  );
+};
+
+export default SalesPolicy;
