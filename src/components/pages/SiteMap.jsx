b2b02c659d0931521fb3f65c058924923d3472fe
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-05-09 02:07:43 -0300

    SMP Component; (#024)

---
+++ src/components/pages/SiteMap.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../layouts/Navbar";
+import Footer from "../Footer";
+
+const SiteMap = () => {
+  return (
+    <div>
+      <Navbar />
+      SiteMap
+      <Footer />
+    </div>
+  );
+};
+
+export default SiteMap;
