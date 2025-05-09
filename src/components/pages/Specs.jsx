d00504eea0c33677e8f232ce20f8864dbb2d7791
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-05-09 02:05:03 -0300

    Specs @AleRzendee

---
+++ src/components/pages/Specs.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../layouts/Navbar";
+import Footer from "../Footer";
+import FloatingNavbar from "../FloatingNavbar";
+
+const Specs = () => {
+  return (
+    <div>
+      <Navbar />
+      <FloatingNavbar />
+      Specs
+      <Footer />
+    </div>
+  );
+};
+
+export default Specs;
