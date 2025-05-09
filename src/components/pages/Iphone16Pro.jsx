68afb90145093f3c62b49d1dda972805cdd54c48
Alexandre Rezende <gustavo.alesalrez@outlook.com>
2025-05-09 02:04:46 -0300

    Phone Component; (#018)

---
+++ src/components/pages/Iphone16Pro.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../layouts/Navbar";
+import Footer from "../Footer";
+import FloatingNavbar from "../FloatingNavbar";
+
+const Iphone16Pro = () => {
+  return (
+    <div>
+      <Navbar />
+      <FloatingNavbar />
+      Iphone16Pro
+      <Footer />
+    </div>
+  );
+};
+
+export default Iphone16Pro;
